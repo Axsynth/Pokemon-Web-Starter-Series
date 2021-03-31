f14fd3eb30a9c2d110484118cf92ae1ba94925c8
Axsynth <axsynth@gmail.com>
2021-03-31 01:25:26 -0400

    Creating a crude, simple API of 1 object. Add project files.

---
+++ Pokemon Web Starter Series/Controllers/PokemonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokemon_Web_Starter_Series
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        public List<Pokemon> Get()
+        {
+            var MyList = new List<Pokemon>();
+
+            var Bulbasaur = new Pokemon();
+            Bulbasaur.Number = 1;
+            Bulbasaur.Name = "Bulbasaur";
+
+            MyList.Add(Bulbasaur);
+
+            return MyList;
+        }
+    }
+}
