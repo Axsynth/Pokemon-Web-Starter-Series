eb3b3ac0e00d2d162d236338c59121ef0f86d380
Axsynth <axsynth@gmail.com>
2021-04-01 16:35:45 -0400

    Removed starter app. Update Pokemon attributes.

--- Pokemon Web Starter Series/Models/Pokemon.cs
+++ Pokemon Web Starter Series/Models/Pokemon.cs
@@ -9,5 +9,7 @@ namespace Pokemon_Web_Starter_Series
     {
         public int Number { get; set; }
         public string Name { get; set; }
+        public string Type1 { get; set; }
+        public string Type2 { get; set; }
     }
 }
